eaee0be875d2c065f1734d23e84fec281650d3b1
ravi-dafauti <34746281+ravi-dafauti@users.noreply.github.com>
2018-01-04 21:39:37 +0530

    find root nth node in Linked List if N(length of List) is not known

---
+++ LinkedList/rootNnode.cpp
@@ -0,0 +1,70 @@
+// FindKthelementFromEnd.cpp : Defines the entry point for the console application.
+
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node* next;
+};
+
+void rootNnode(Node **head_ref)
+{
+	int i = 1;
+	int j = 1;
+	Node *rootnode = NULL;
+	Node *temp = *head_ref;
+	while (temp)
+	{
+		if (i == j*j)
+		{
+			if (rootnode == NULL)
+				rootnode = *head_ref;
+			else
+				rootnode = rootnode->next;
+			j++;
+		}
+		i++;
+		temp = temp->next;
+	}
+
+	cout << "root nth node is : " << rootnode->data << "\n";
+}
+
+
+void push(Node** head_ref, int new_data)
+{
+	Node* new_node = (Node*)malloc(sizeof(Node));
+
+	new_node->data = new_data;
+	new_node->next = (*head_ref);
+
+	(*head_ref) = new_node;
+}
+
+void printList(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d  ", temp->data);
+		temp = temp->next;
+	}
+}
+
+int main()
+{
+	struct Node* head = NULL;
+
+	push(&head, 20);
+	push(&head, 4);
+	push(&head, 15);
+	push(&head, 85);
+
+	printf("Given linked list\n");
+	printList(head);
+
+	rootNnode(&head);
+	return 0;
+}
