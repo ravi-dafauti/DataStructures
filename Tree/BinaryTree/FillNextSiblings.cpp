5a7e594979d2bbe76df8fc0c5719511b755bef9f
ravi-dafauti <34746281+ravi-dafauti@users.noreply.github.com>
2018-01-14 20:34:58 +0530

    Add files via upload

---
+++ Tree/BinaryTree/FillNextSiblings.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node *left;
+	Node *right;
+	Node *nextSibling;
+};
+
+class Tree
+{
+	Node *root;
+public:
+	Node *rootOfTree();
+	void copyTree(Node *);
+	void fillNextSiblingsIterative();
+	void fillNextSiblingsRecursive(Node *);
+};
+
+void Tree::fillNextSiblingsIterative()
+{
+	queue<Node *> aux_queue;
+	if (root == NULL)
+		return;
+	aux_queue.push(root);
+	aux_queue.push(NULL);
+	while (!aux_queue.empty())
+	{
+		Node *temp = aux_queue.front();
+		aux_queue.pop();
+		if (temp != NULL)
+		{
+			temp->nextSibling = aux_queue.front();
+			if (temp->left != NULL)
+				aux_queue.push(temp->left);
+			if (temp->right != NULL)
+				aux_queue.push(temp->right);
+		}
+		else
+		{
+			if (!aux_queue.empty())
+			{
+				aux_queue.push(NULL);
+			}
+		}
+	}
+}
+
+void Tree::fillNextSiblingsRecursive(Node *root_ref)
+{
+	if (root_ref == NULL)
+		return;
+	if (root_ref == root)
+		root_ref->nextSibling = NULL;
+	if (root_ref->left != NULL)
+		root_ref->left->nextSibling = root_ref->right;
+	if (root_ref->right != NULL && root_ref->nextSibling != NULL)
+		root_ref->right->nextSibling = root_ref->nextSibling->left;
+	else
+		root_ref->right->nextSibling = NULL;
+	fillNextSiblingsRecursive(root->left);
+	fillNextSiblingsRecursive(root->right);
+}
+Node *Tree::rootOfTree()
+{
+	return root;
+}
+
+void Tree::copyTree(Node *root_ref)
+{
+	root = root_ref;
+}
+
+Node * newNode(int data)
+{
+	Node * node = (Node*)malloc(sizeof(Node));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+
+	return(node);
+}
+int main()
+{
+	Node *root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	root->right->left = newNode(6);
+	root->right->right = newNode(7);
+
+	Tree mTree;
+	mTree.copyTree(root);
+	return 0;
+}
