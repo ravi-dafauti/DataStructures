48b0086ffba61b3de7ded020e99cd08bac15565e
ravinder dafauti <34746281+ravi-dafauti@users.noreply.github.com>
2018-05-21 20:07:15 +0530

    Sub Matrix Sum Queries

---
+++ Matrix/SubmatrixSumQuerries.cpp
@@ -0,0 +1,97 @@
+/*
+Given a matrix of size M x N, there are large number of queries to find submatrix sums. Inputs to queries are left top and right bottom indexes of submatrix whose sum is to find out.
+
+How to preprocess the matrix so that submatrix sum queries can be performed in O(1) time.
+
+Example :
+
+tli :  Row number of top left of query submatrix
+tlj :  Column number of top left of query submatrix
+rbi :  Row number of bottom right of query submatrix
+rbj :  Column number of bottom right of query submatrix
+
+Input: mat[M][N] = {{1, 2, 3, 4, 6},
+{5, 3, 8, 1, 2},
+{4, 6, 7, 5, 5},
+{2, 4, 8, 9, 4} };
+Query1: tli = 0, tlj = 0, rbi = 1, rbj = 1
+Query2: tli = 2, tlj = 2, rbi = 3, rbj = 4
+Query3: tli = 1, tlj = 2, rbi = 3, rbj = 3;
+
+Output:
+Query1: 11  // Sum between (0, 0) and (1, 1)
+Query2: 38  // Sum between (2, 2) and (3, 4)
+Query3: 38  // Sum between (1, 2) and (3, 3)
+*/
+
+// C++ program to compute submatrix query sum in O(1)
+// time
+#include<iostream>
+using namespace std;
+#define M 4
+#define N 5
+ 
+// Function to preprcess input mat[M][N].  This function
+// mainly fills aux[M][N] such that aux[i][j] stores sum
+// of elements from (0,0) to (i,j)
+void preProcess(int mat[M][N], int aux[M][N])
+{
+	for (int i = 0; i < N; i++)
+		aux[0][i] = mat[0][i];
+	for (int i = 1; i < M; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			aux[i][j] = mat[i][j] + aux[i - 1][j];
+		}
+	}
+	for (int i = 0; i < M; i++)
+	{
+		for (int j = 1; j < N; j++)
+		{
+			aux[i][j] += aux[i][j - 1];
+		}
+	}
+}
+ 
+// A O(1) time function to compute sum of submatrix
+// between (tli, tlj) and (rbi, rbj) using aux[][]
+// which is built by the preprocess function
+int sumQuery(int aux[M][N], int tli, int tlj, int rbi, int rbj)
+{
+	int res = aux[rbi][rbj];
+	if (tli - 1 >= 0)
+	{
+		res -= aux[tli - 1][rbj];
+	}
+	if (tlj - 1 >= 0)
+		res -= aux[rbi][tlj - 1];
+	if (tli - 1 >= 0 && tlj - 1 >= 0)
+		res += aux[tli - 1][tlj - 1];
+	return res;
+}
+ 
+// Driver program
+int main()
+{
+   int mat[M][N] = {{1, 2, 3, 4, 6},
+                    {5, 3, 8, 1, 2},
+                    {4, 6, 7, 5, 5},
+                    {2, 4, 8, 9, 4} };
+   int aux[M][N];
+ 
+   preProcess(mat, aux);
+ 
+   int tli = 2, tlj = 2, rbi = 3, rbj = 4;
+   cout << "\nQuery1: " << sumQuery(aux, tli, tlj, rbi, rbj);
+ 
+   tli = 0, tlj = 0, rbi = 1, rbj = 1;
+   cout << "\nQuery2: " << sumQuery(aux, tli, tlj, rbi, rbj);
+ 
+   tli = 1, tlj = 2, rbi = 3, rbj = 3;
+   cout << "\nQuery3: " << sumQuery(aux, tli, tlj, rbi, rbj);
+ 
+   return 0;
+}
+
+
